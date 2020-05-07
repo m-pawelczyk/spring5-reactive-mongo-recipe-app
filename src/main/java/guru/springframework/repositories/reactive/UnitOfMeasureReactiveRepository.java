3f741fe896967e983901c988d78cf8f0d81cb74a
Michał Pawełczyk <michal.pawelczyk@gmail.com>
2020-05-07 14:38:06 +0200

    start refactor types to reactive, add temporary test for UnitOfMeasureReactiveRepository.java to check that it is working

---
+++ src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -0,0 +1,13 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+/**
+ * m-pawelczyk (GitGub) / m_pawelczyk (Twitter)
+ * on 07.05.2020
+ * created UnitOfMeasureReactiveRepository in guru.springframework.repositories.reactive
+ * in project spring5-reactive-mongo-recipe-app
+ */
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+}
